964557ff191c2ebb88c4fba94f811d53cd5db535
Chitranshu2512 <118387802+Chitranshu2512@users.noreply.github.com>
2023-07-22 15:59:05 +0530

    Add files via upload

---
+++ CheckPalindrome.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class CheckPalindrome{
+    static boolean checkPalindrome(String s)
+    {
+        s = s.toLowerCase();
+        int l = 0;
+        int r = s.length() -1;
+        while (l<r)
+        {
+            if(s.charAt(l) == s.charAt(r))
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        String s= "MADAVI";
+        String s2= "CIPHER";
+        String s3 = "Madam";
+
+        System.out.println(checkPalindrome(s));
+        System.out.println(checkPalindrome(s2));
+        System.out.println(checkPalindrome(s3));
+
+    }
+}
+
